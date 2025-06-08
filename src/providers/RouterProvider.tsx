c6b1960dc06c15b62c8b17d73c9975bc41a4368c
Gabriel Domingos Peixoto Pimenta <gabrieldomingos@digix.com.br>
2025-06-08 19:32:03 -0300

    rename folder to fix name wrong, authentication module

--- src/providers/RouterProvider.tsx
+++ src/providers/RouterProvider.tsx
@@ -6,10 +6,10 @@ import {
 import PublicLayout from "@layouts/PublicLayout";
 import MainLayout from "@layouts/MainLayout";
 
-import Login from "@modules/authentication/Login";
-import Register from "@modules/authentication/Register";
+import Login from "@modules/authentication_rename/Login";
+import Register from "@modules/authentication_rename/Register";
 import Home from "@modules/home";
-import ConfirmEmail from "@modules/authentication/ConfirmEmail";
+import ConfirmEmail from "@modules/authentication_rename/ConfirmEmail";
 import ForgotPassword from "@modules/recover-password/ForgotPassword";
 import RecoverPassword from "@modules/recover-password/RecoverPassword";
 
