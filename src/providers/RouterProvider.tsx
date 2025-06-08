8adbdfe90a9ce893a85f489687fccddee62c60e5
Gabriel Domingos Peixoto Pimenta <gabrieldomingos@digix.com.br>
2025-06-08 19:33:34 -0300

    fix names of home and authentication modules

--- src/providers/RouterProvider.tsx
+++ src/providers/RouterProvider.tsx
@@ -6,12 +6,12 @@ import {
 import PublicLayout from "@layouts/PublicLayout";
 import MainLayout from "@layouts/MainLayout";
 
-import Login from "@modules/authentication_rename/Login";
-import Register from "@modules/authentication_rename/Register";
-import Home from "@modules/home";
-import ConfirmEmail from "@modules/authentication_rename/ConfirmEmail";
+import Login from "@modules/authentication/Login";
+import Register from "@modules/authentication/Register";
+import ConfirmEmail from "@modules/authentication/ConfirmEmail";
 import ForgotPassword from "@modules/recover-password/ForgotPassword";
 import RecoverPassword from "@modules/recover-password/RecoverPassword";
+import Home from "@modules/Home_rename";
 
 export const routePaths = {
   initial: "/",
